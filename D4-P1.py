c40f993c0e3bd4de3d865b0d577827d833dad529
HeroAtomic <57571896+HeroAtomic@users.noreply.github.com>
2019-12-09 18:05:09 -0700

    Add files via upload

--- D4-P1.py
+++ D4-P1.py
@@ -11,14 +11,17 @@ valid_passwords = []
 
 print('Checking...')
 
-for i in range (start, stop):
-    i = str(i)
-    if i[0] == i[1] or i[1] == i[2] or i[2] == i[3] or i[3] == i[4] or i[4] == i[5]:
-        if i[0] <= i[1] and i[1] <= i[2] and i[2] <= i[3] and i[3] <= i[4] and i[4] <= i[5]:
-            valid_passwords.append(i)
+def possible_passwords():
+    for i in range (start, stop):
+        i = str(i)
+        if i[0] == i[1] or i[1] == i[2] or i[2] == i[3] or i[3] == i[4] or i[4] == i[5]:
+            if i[0] <= i[1] and i[1] <= i[2] and i[2] <= i[3] and i[3] <= i[4] and i[4] <= i[5]:
+                valid_passwords.append(i)
+            else:
+                pass
         else:
             pass
-    else:
-        pass
+    return valid_passwords
 
+valid_passwords = possible_passwords()
 print('Number of valid passwords:', len(valid_passwords))
