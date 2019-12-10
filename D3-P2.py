94361778441660c328e4cda4a622ddfd7f7aebe8
HeroAtomic <57571896+HeroAtomic@users.noreply.github.com>
2019-12-09 17:37:37 -0700

    Add files via upload

--- D3-P2.py
+++ D3-P2.py
@@ -1,8 +1,9 @@
-# Me: Use functions you moron and don't just spagetti nest everything :)
-# Me: Ok
+# Part2 solution broke part 1?
+
 import csv
 from copy import deepcopy
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 start_time = datetime.now()
 
@@ -79,32 +80,28 @@ def find_full_path(wire):
             i2 += 1
 
     except:
-        print('Wire 1 path traced')
+        print('Wire path traced')
     return current_positions
 
 def find_intersections():
     intersections = []
-    total_steps_list = []
-
-    print(w1)
-    print(w2)
+    steps = {}
 
     for i in w1:
         if i in w2:
+            # Intersections
             print('Intersection found at:', i)
-
-            w1_steps = w1.index(i) + 1
-            w2_steps = w2.index(i) + 1
-
-            # find the steps
-            print('w1 steps:', w1_steps)
-            print('w2 steps:', w2_steps)
-
-            # Total_steps for checking
-            total_steps = w1_steps + w2_steps
-            total_steps_list.append(total_steps)
             intersections.append(i)
-    return intersections, total_steps_list
+
+            # Steps to intersection
+            #print(w1)
+            #print(w2)
+            w1_len = w1.index(i) + 1
+            w2_len = w2.index(i) + 1
+            total = w1_len + w2_len
+            #print('Distance:', total)
+            steps[str(i)] = total
+    return intersections, steps
 
 def find_manhattan():
     distances = []
@@ -114,23 +111,23 @@ def find_manhattan():
         distances.append(dist)
 
     #distances.sort()
-    return distances[1]
+    return distances[0]
 
 
-wire1 = import_list('wire1_input_test.txt')
-wire2 = import_list('wire2_input_test.txt')
+wire1 = import_list('wire1_input.txt')
+wire2 = import_list('wire2_input.txt')
 
-wire1 = ['R8', 'U5', 'L5', 'D3']
-wire2 = ['U7', 'R6', 'D4', 'L4']
+#wire1 = ['R8', 'U5', 'L5', 'D3']
+#wire2 = ['U7', 'R6', 'D4', 'L4']
 
 # find the turning point of each wire
-print('Finding path')
+#print('Finding path')
 wire1_pos = find_position(wire1)
-#wire1_pos.insert(0, [0, 0])
+wire1_pos.insert(0, [0, 0])
 #print('wire1 pos', wire1_pos)
 
 wire2_pos = find_position(wire2)
-#wire2_pos.insert(0, [0, 0])
+wire2_pos.insert(0, [0, 0])
 #print('wire2 pos', wire2_pos)
 
 
@@ -141,19 +138,20 @@ w2 = find_full_path(wire2_pos)
 
 # Get the intersection points
 print('Getting intersection points:')
-intersections, total_steps = find_intersections()
+intersections, steps = find_intersections()
+
+key_min = min(steps.keys(), key=(lambda k: steps[k]))
+steps_min = steps[key_min]
 
 # Get the manhattan distance (the sum of the absolute values)
 shortest_distance = find_manhattan()
 
 print('\n')
 print('Shortest path found!!')
-print('The shortest distance to the intersection point is: {}'.format(shortest_distance))
-print('The shortest number of grid steps is: {}'.format(total_steps[0]))
-print(total_steps)
+print('The shortest manhattan distance to the intersection point is: {}'.format(shortest_distance))
+print('Shortest amount of combined steps is: {}'.format(steps_min))
 
 end_time = datetime.now()
 
-
-# print('Start time: {}'.format(start_time))
-# print('End time: {}'.format(end_time))
+print('Start time: {}'.format(start_time))
+print('End time: {}'.format(end_time))
